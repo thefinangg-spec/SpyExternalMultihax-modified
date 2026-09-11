4dd664a9fee483d86f4abf3f0e8106247f91c365
thefinangg-spec <thefinangg@gmail.com>
2026-09-11 07:34:03 -0700

    Update main.h

--- cheat1/main.h
+++ cheat1/main.h
@@ -8,7 +8,6 @@
 #pragma warning( disable : 4996 )
 #include <iostream>
 #include <d3dx9.h>
-#include <d3d9.h>
 #pragma comment(lib, "d3d9.lib")
 #pragma comment(lib, "d3dx9.lib")
 #include <dwmapi.h>
